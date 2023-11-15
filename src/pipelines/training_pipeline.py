3103b218e3f1945caa0f5538689cb3e3310c7165
Shriansh16 <shrianshsingh20@gmail.com>
2023-11-15 13:30:00 +0530

    first commit

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -9,12 +9,11 @@ from components.prepering_base_model import *
 from components.training import *
 
 
+
 if __name__=='__main__':
-    config = ConfigurationManager()
-    prepare_base_model_config = config.get_prepare_base_model_config()
-    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
-    prepare_base_model.get_base_model()
-    prepare_base_model.update_base_model()
-   
-    
-    
+    config2 = ConfigurationManager2()
+    training_config = config2.get_training_config()
+    training = Training(config=training_config)
+    training.get_base_model()
+    training.train_valid_generator()
+    training.train()
